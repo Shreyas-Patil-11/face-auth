303660cfac6cf97412c5da1250f0348d357a98ff
Shreyas-Patil-11 <shreyaspatil1311@gmail.com>
2025-02-22 00:07:00 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -140,6 +140,6 @@ app.post('/login', upload.single('image'), async (req, res) => {
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// const PORT = process.env.PORT || 5000; 
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
